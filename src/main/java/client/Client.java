27b9956709ae0b23c3858a723c5705ec42817635
Fernando Pires <ferjpires@gmail.com>
2024-11-12 17:14:19 +0000

    gradle builder

---
+++ src/main/java/client/Client.java
@@ -0,0 +1,10 @@
+package client;
+
+public class Client {
+    public static void main(String[] args) {
+        System.out.println("hello world from client");
+    }
+    public void hello(){
+        System.out.println("hello");
+    }
+}
